424ea22517cb9e0b853606787a8a3610cb25f7f6
Anders Granlund <granlund23@yahoo.se>
2025-01-22 17:27:58 +0100

    Added sound commandline options
    Also build for 020-060 instead of just 060

--- sys/atari/atari-sound.c
+++ sys/atari/atari-sound.c
@@ -22,11 +22,12 @@ extern int  pcm_submit_sdl();
 extern void pcm_sync_sdl();
 extern void pcm_pause_sdl(int dopause);
 
-void (*atari_pcm_close)(void);
-int  (*atari_pcm_submit)(void);
-void (*atari_pcm_sync)(void);
-void (*atari_pcm_pause)(int);
-
+void pcm_unassigned0() { }
+int  pcm_unassigned1() { return 1; }
+void (*atari_pcm_close)(void) = pcm_unassigned0;
+int  (*atari_pcm_submit)(void) = pcm_unassigned1;
+void (*atari_pcm_sync)(void) = pcm_unassigned0;
+void (*atari_pcm_pause)(int) = pcm_unassigned0;
 
 /* -----------------------------------------------------------------------------------
  * 
@@ -36,27 +37,39 @@ void (*atari_pcm_pause)(int);
 
 struct pcm pcm;
 
+static int sound = 1;
+static int sound_null = 1;
+static int sound_sdl = 1;
+static int sound_sb = 1;
+
 rcvar_t pcm_exports[] =
 {
+    RCV_BOOL("sound", &sound, "sound enabled"),
+#if SOUND_ENABLE_SOUNDBLASTER
+    RCV_BOOL("sbsound",   &sound_sb,  "enable soundblaster driver"),
+#endif
+#if SOUND_ENABLE_SDL
+    RCV_BOOL("sdlsound",  &sound_sdl, "enable sdl sound driver"),
+#endif    
+    RCV_BOOL("nullsound", &sound_null, "enable null sound driver"),
 	RCV_END
 };
 
-
 void pcm_init()
 {
-    if (1 && pcm_init_sb()) {
+    if (SOUND_ENABLE_SOUNDBLASTER && sound && sound_sb && pcm_init_sb()) {
         atari_pcm_close = pcm_close_sb;
         atari_pcm_submit = pcm_submit_sb;
         atari_pcm_sync = pcm_sync_sb;
         atari_pcm_pause = pcm_pause_sb;
     }
-    else if (1 && pcm_init_sdl()) {
+    else if (SOUND_ENABLE_SDL && sound && sound_sdl && pcm_init_sdl()) {
         atari_pcm_close = pcm_close_sdl;
         atari_pcm_submit = pcm_submit_sdl;
         atari_pcm_sync = pcm_sync_sdl;
         atari_pcm_pause = pcm_pause_sdl;
     }
-    else if (1 && pcm_init_null()) {
+    else if (sound_null && pcm_init_null()) {
         atari_pcm_close = pcm_close_null;
         atari_pcm_submit = pcm_submit_null;
         atari_pcm_sync = pcm_sync_null;
